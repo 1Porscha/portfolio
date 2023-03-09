47fe8d245a64bc2473731d3e8408afddf72785b6
1Porscha <117795389+1Porscha@users.noreply.github.com>
2023-03-08 23:11:38 -0500

    link

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import {Link} from 'react-router-dom'
 
 function App() {
   
-
+  
   
 
   return (
     <div className="App">
       
       <h1>Portfolio</h1>
-      <h3>Email: porscha_osler@yahoo.com</h3>
-      <Link to='https://github.com/1Porscha'><h3>Github</h3></Link>
-      <Link to='www.linkedin.com/in/porschaosler'><h3>Linkedin Page</h3></Link>
-      <Link to='https://dashboard.render.com/web/srv-cg4fnokeoogtrlsdcc1g'><h3>Render</h3></Link>
+      <h2>Email: porscha_osler@yahoo.com</h2>
+      <Link to='https://github.com/1Porscha'><h2>Github</h2></Link>
+      <Link to='https://trading-criteria-checklist.onrender.com/'><h2>Render</h2></Link>
+      
     </div>
   );
 }
